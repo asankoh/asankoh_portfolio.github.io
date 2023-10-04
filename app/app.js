2fedc541a183f7bebda32d03c253aa57f3029381
asankoh <89537452+asankoh@users.noreply.github.com>
2023-10-04 09:56:25 -0400

    new content

--- app/app.js
+++ app/app.js
@@ -10,21 +10,85 @@ const iconsAndLabels = [
   // Add more entries as needed
 ];
 
- $(document).ready(function () {
 
+function iconsAndLabels_loop(){
   const flexContainer = $('#flexContainer');
-
+  
   function createIconElement(icon, label) {
     return $(`
-      <div class="icon-wrapper">
-        <div class="icon"><i class="${icon}"></i></div>
-        <div class="icon-label">${label}</div>
-      </div>
+    <div class="icon-wrapper">
+    <div class="icon"><i class="${icon}"></i></div>
+    <div class="icon-label">${label}</div>
+    </div>
     `);
   }
-
+  
   iconsAndLabels.forEach(({ icon, label }) => {
     const iconElement = createIconElement(icon, label);
     flexContainer.append(iconElement);
   });
+}
+
+emailjs.init("7Sq14UgLg4xUzGRL-")
+
+$(document).ready(function() {
+  $('.submitContact').click(function() {
+    if (validateInputs()) {
+      sendEmail();
+    } else {
+      Swal.fire(
+        'Input Error',
+        'Please fill in all the required fields.',
+        'error'
+      );
+    }
+  });
+
+  function validateInputs() {
+    const name = $(".nameInput").val();
+    const email = $(".emailInput").val();
+    const subject = $(".subjectInput").val();
+    const message = $(".messageBox").val();
+
+    // Check if any of the input fields is empty
+    if (name === "" || email === "" || subject === "" || message === "") {
+      return false;
+    }
+    return true;
+  }
+
+  function sendEmail() {
+    const name = $(".nameInput").val();
+    const email = $(".emailInput").val();
+    const subject = $(".subjectInput").val();
+    const message = $(".messageBox").val();
+
+    emailjs.send("service_lpcn1ie", "template_8efq0pg", {
+      from_name: name,
+      from_email: email,
+      subject: subject,
+      message: message
+    }).then(function() {
+      Swal.fire(
+        'Thanks!',
+        'You successfully sent an email!',
+        'success'
+      );
+      $(".nameInput").val("");
+      $(".emailInput").val("");
+      $(".subjectInput").val("");
+      $(".messageBox").val("");
+    }, function(error) {
+      Swal.fire(
+        'Email Error',
+        'Failure to send email. Please try again later.',
+        'error'
+      );
+    });
+  }
+});
+
+ $(document).ready(function () {
+  iconsAndLabels_loop();
+  sendEmail();
 });
